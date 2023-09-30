7034586d19ebe1038d09cbbf4824201d097e076a
Isailton89 <isailton2008@gmail.com>
2023-09-30 11:38:38 -0300

    inserindo variaveis e o Scanner

--- src/ContaTerminal.java
+++ src/ContaTerminal.java
@@ -1,5 +1,12 @@
+import java.util.Scanner;
+
 public class ContaTerminal {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        int numeroDaConta;
+        String agencia;
+        String nomeCliente;
+        double saldo;
+
+        Scanner entradaDados = new Scanner(System.in);
     }
 }
